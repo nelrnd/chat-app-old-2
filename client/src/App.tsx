68cc7a83765e3b9e91acb581bb5e285fb1deb566
Nel <nel.renaudin@gmail.com>
2024-04-16 12:04:02 +0200

    install tailwindcss and shadcn-ui in client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Hello React</h1>
+      <Button>Coucou</Button>
+    </>
+  )
+}
+
+export default App
